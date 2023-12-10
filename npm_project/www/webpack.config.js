aa9d218f22d59d7f5fda9536f8ea497e2deadb73
kashiwara <kashiwara@kashiwaranoMacBook-Air.local>
2023-12-10 13:20:21 +0900

    webpback-dev-serverが立ち上がるようにした

--- npm_project/www/webpack.config.js
+++ npm_project/www/webpack.config.js
@@ -3,6 +3,14 @@ const path = require('path');
 
 module.exports = {
   entry: "./bootstrap.js",
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true,
+    open: true,
+    port: 8080,
+    host: '0.0.0.0',
+    historyApiFallback: true,
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
