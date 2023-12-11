acc451a689bb9ff125c2fe557c4a3952c97f0081
kashiwara <kashiwara@kashiwaranoMacBook-Air.local>
2023-12-11 20:49:54 +0900

    life gameが稼働するところまで実装

--- npm_project/www/index.js
+++ npm_project/www/index.js
@@ -1,3 +1,12 @@
-import * as wasm from "./pkg/wasm_game_of_life"
+import { Universe } from "./pkg/wasm_game_of_life"
 
-wasm.greet();
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+    requestAnimationFrame(renderLoop);
+  };
+
+requestAnimationFrame(renderLoop);
